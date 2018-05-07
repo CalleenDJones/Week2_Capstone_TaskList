eab0c5700bcf3d1ccb85e504953c7b4888a037b8
Calleen D. Jones <calleendjones@gmail.com>
2018-05-07 09:50:00 -0400

    First Commit, incomplete

---
+++ Week2_Capstone_TaskList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Capstone_TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a new Task object
+            Task newTask = new Task("Keith", "Garbage Duty");
+            Task secondTask = new Task("Jacob", "Dishes");
+
+
+            //Print the different Objects
+            Console.WriteLine(newTask.TaskOwner + " " 
+                + newTask.TaskName + " " + newTask.DueDate);
+            Console.WriteLine(secondTask.TaskOwner + " " 
+                + secondTask.TaskName+ " " + newTask.DueDate);
+
+            //create list
+            List<Task> TaskList = new List<Task>();
+
+            //We want to build a new list of tasks, so
+            //  we're going to create a loop
+
+            bool whileBool = false;
+            string response = " ";
+
+            //add as many tasks as you want with this
+            while (whileBool)
+            {
+                TaskList.Add( new Task(Console.ReadLine(), Console.ReadLine());
+                Console.WriteLine("quit");
+                response = Console.ReadLine();
+
+                if (response == "quit")
+                    whileBool = false;
+            }
+
+            //pritn the tasks from the task List
+            foreach (Task item in TaskList)
+            {
+                Console.WriteLine(item.TaskOwner + " " + item.TaskName);
+
+            }
+        }
+    }
+}
---
+++ Week2_Capstone_TaskList/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Capstone_TaskList
+{
+    class Task
+
+    {
+        //Classes allow us to make Objects
+        //We make them public so that we can access this Class 
+        //  within our Program.cs Program Class --> class Program
+        public string TaskOwner { get; set; }
+        public string TaskName { get; set; }
+        public DateTime DueDate { get; set; }
+        
+
+        //Begin creating Constructors
+        //Constructor names should be in Camel Case
+        //We don't leave constructors at the default() because
+        public Task(string taskOwner, string taskName)
+        {
+            //assign the Taskname to the Constructor string
+            TaskOwner = taskOwner;
+            TaskName = taskName;
+            DueDate = DateTime.Now.AddDays(14);
+        }
+
+
+       
+    }
+}
